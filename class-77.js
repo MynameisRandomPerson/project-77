9e26cdeeda0042769e29089481f5ea9339e66a47
MynameisRandomPerson <89684390+MynameisRandomPerson@users.noreply.github.com>
2023-01-22 19:13:04 +1100

    Add files via upload

---
+++ class-77.js
@@ -0,0 +1,21 @@
+
+    var bignumber = 8;
+    var smallnumber = 2;
+    var firstname = "Sparsh";
+    var lastname = "Kunwar";
+    var fullname = firstname.concat(lastname);
+    var plus = bignumber + smallnumber
+    var sub = bignumber - smallnumber;
+    var multiple = bignumber * smallnumber;
+    var divide = bignumber / smallnumber;
+    
+
+    console.log(fullname);
+    console.log(plus);
+    console.log(sub);
+    console.log(multiple);
+    console.log(divide);
+
+
+
+    
